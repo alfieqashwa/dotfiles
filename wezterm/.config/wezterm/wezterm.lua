521cbf95493985f2d55510e78821a9fcab7f547b
Alfie Qashwa <alfieqashwa@gmail.com>
2025-04-01 16:23:36 +0700

    first commit

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,63 @@
+local wezterm = require("wezterm")
+
+return {
+	keys = {
+		-- Vertical Split
+		{
+			key = "\\",
+			mods = "CTRL",
+			action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+		},
+		-- Horizontal Split
+		{
+			key = "|",
+			mods = "CTRL",
+			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+		},
+	},
+
+	--default path
+	default_cwd = "/home/alfieqashwa/Code/",
+	-- Fonts
+	font = wezterm.font("FiraCode Nerd Font", { weight = "Bold", italic = false }),
+	font_size = 12.0,
+	line_height = 1.0,
+
+	-- Colors
+	bold_brightens_ansi_colors = true,
+	inactive_pane_hsb = {
+		hue = 1.0,
+		saturation = 0.7,
+		brightness = 1.0,
+	},
+
+	-- Backgrounds
+	-- 619738
+	window_background_image = "/usr/share/backgrounds/enddeavourOS.jpeg",
+	-- window_background_image = "/home/alfieqashwa/Downloads/blur.jpg",
+	-- window_background_image = "/home/alfieqashwa/Pictures/Radiohead-wallpaper.jpg",
+	-- window_background_opacity = 1.0,
+	window_background_image_hsb = {
+		-- Darken the background image by reducing it to 1/3rd
+		brightness = 0.1,
+
+		-- You can adjust the hue by scaling its value.
+		-- a multiplier of 1.0 leaves the value unchanged.
+		hue = 0.1,
+
+		-- You can adjust the saturation also.
+		saturation = 3.0,
+	},
+
+	-- color_scheme = "Firewatch",
+	-- color_scheme = "Banana Blueberry",
+	-- color_scheme = "Bright Lights",
+	-- color_scheme = "Calamity",
+	-- color_scheme = "Catppuccin Mocha (Gogh)",
+	-- color_scheme = "Duotone Dark",
+	color_scheme = "Chalk",
+	scrollback_lines = 3500,
+	default_prog = { "/bin/fish" },          -- set default shell to fish
+	hide_mouse_cursor_when_typing = true,    -- hide mouse cursor when typing in the panel
+	default_cursor_style = "SteadyUnderline", -- default cursor style is underline
+}
