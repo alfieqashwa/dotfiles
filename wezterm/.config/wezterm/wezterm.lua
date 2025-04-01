fecfd54689a7504f64aa910e3d223f959e6af725
Alfie Qashwa <alfieqashwa@gmail.com>
2025-04-01 18:16:56 +0700

    modified wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -18,9 +18,12 @@ return {
 
 	--default path
 	default_cwd = "/home/alfieqashwa/Code/",
+	adjust_window_size_when_changing_font_size = false,
 	-- Fonts
 	font = wezterm.font("FiraCode Nerd Font", { weight = "Bold", italic = false }),
-	font_size = 12.0,
+	-- font_size = 12.0,
+	font_size = 13.0,
+	enable_tab_bar = false,
 	line_height = 1.0,
 
 	-- Colors
@@ -32,11 +35,13 @@ return {
 	},
 
 	-- Backgrounds
-	-- 619738
 	window_background_image = "/usr/share/backgrounds/enddeavourOS.jpeg",
 	-- window_background_image = "/home/alfieqashwa/Downloads/blur.jpg",
 	-- window_background_image = "/home/alfieqashwa/Pictures/Radiohead-wallpaper.jpg",
-	-- window_background_opacity = 1.0,
+	window_background_opacity = 1.0,
+	window_decoration = "RESIZE",
+	-- macos_window_background_blur = 30,
+
 	window_background_image_hsb = {
 		-- Darken the background image by reducing it to 1/3rd
 		brightness = 0.1,
@@ -46,7 +51,7 @@ return {
 		hue = 0.1,
 
 		-- You can adjust the saturation also.
-		saturation = 3.0,
+		saturation = 4.0,
 	},
 
 	-- color_scheme = "Firewatch",
@@ -56,6 +61,7 @@ return {
 	-- color_scheme = "Catppuccin Mocha (Gogh)",
 	-- color_scheme = "Duotone Dark",
 	color_scheme = "Chalk",
+
 	scrollback_lines = 3500,
 	default_prog = { "/bin/fish" },          -- set default shell to fish
 	hide_mouse_cursor_when_typing = true,    -- hide mouse cursor when typing in the panel
