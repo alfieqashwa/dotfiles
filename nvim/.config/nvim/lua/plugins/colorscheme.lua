eb1972ba02b7c437e33b86f9cee04d3e6717a5ba
Alfie Qashwa <alfieqashwa@gmail.com>
2025-04-03 02:17:00 +0700

    update priority colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,23 @@
 return {
   {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = true,
-    name = "solarized-osaka",
-    priority = 1000,
-    opts = {
-      transparent = true,
-    },
-  },
-  {
     "folke/tokyonight.nvim",
     lazy = true,
+    priority = 1000,
     opts = {
       transparent = true,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
       },
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    name = "solarized-osaka",
+    -- priority = 1000,
+    opts = {
+      transparent = true,
     },
   },
 }
