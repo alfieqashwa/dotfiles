41261e650ac5a9268c7ea6a05f49b4fd2eed3fda
Alfie Qashwa <alfieqashwa@gmail.com>
2025-04-03 02:13:25 +0700

    updating

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    name = "solarized-osaka",
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -35,24 +35,59 @@ return {
 	},
 
 	-- Backgrounds
-	window_background_image = "/usr/share/backgrounds/enddeavourOS.jpeg",
+	-- window_background_image = "/home/alfieqashwa/Pictures/wezterm-images/bg-blurred-darker.png",
+	-- window_background_image = "/usr/share/backgrounds/enddeavourOS.jpeg",
 	-- window_background_image = "/home/alfieqashwa/Downloads/blur.jpg",
 	-- window_background_image = "/home/alfieqashwa/Pictures/Radiohead-wallpaper.jpg",
-	window_background_opacity = 1.0,
+	window_background_opacity = 0.1,
+	text_background_opacity = 0.4,
+
 	window_decoration = "RESIZE",
+	hide_tab_bar_if_only_one_tab = true,
 	-- macos_window_background_blur = 30,
 
-	window_background_image_hsb = {
-		-- Darken the background image by reducing it to 1/3rd
-		brightness = 0.1,
+	-- window_background_image_hsb = {
+	-- 	-- Darken the background image by reducing it to 1/3rd
+	-- 	brightness = 0.1,
+	--
+	-- 	-- You can adjust the hue by scaling its value.
+	-- 	-- a multiplier of 1.0 leaves the value unchanged.
+	-- 	hue = 0.9,
+	--
+	-- 	-- You can adjust the saturation also.
+	-- 	saturation = 1.0,
+	-- },
 
-		-- You can adjust the hue by scaling its value.
-		-- a multiplier of 1.0 leaves the value unchanged.
-		hue = 0.9,
-
-		-- You can adjust the saturation also.
-		saturation = 1.0,
-	},
+	-- window_background_gradient = {
+	-- 	-- colors = { "deeppink", "gold" },
+	-- 	-- colors = { "#EEBD89", "#D13ABD" },
+	-- 	colors = {
+	-- 		"#0f0c29",
+	-- 		"#302b63",
+	-- 		"#24243e",
+	-- 	},
+	-- 	-- orientation = "Vertical",
+	-- 	orientation = {
+	-- 		Radial = {
+	-- 			-- Specifies the x coordinate of the center of the circle,
+	-- 			-- in the range 0.0 through 1.0.  The default is 0.5 which
+	-- 			-- is centered in the X dimension.
+	-- 			cx = 0.75,
+	--
+	-- 			-- Specifies the y coordinate of the center of the circle,
+	-- 			-- in the range 0.0 through 1.0.  The default is 0.5 which
+	-- 			-- is centered in the Y dimension.
+	-- 			cy = 0.75,
+	--
+	-- 			-- Specifies the radius of the notional circle.
+	-- 			-- The default is 0.5, which combined with the default cx
+	-- 			-- and cy values places the circle in the center of the
+	-- 			-- window, with the edges touching the window edges.
+	-- 			-- Values larger than 1 are possible.
+	-- 			radius = 1.25,
+	-- 		},
+	-- 	},
+	-- },
 
 	-- color_scheme = "Firewatch",
 	-- color_scheme = "Banana Blueberry",
